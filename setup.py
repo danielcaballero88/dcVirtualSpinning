715bff1e6468684cf1d99c400c162196e9627274
Daniel Enrique Caballero <danielcaballero88@gmail.com>
2020-06-16 07:54:25 -0300

    Primera estructura

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dcVirtualSpinning',
+    version='0.1dev',
+    packages=['VirtualSpinning'],
+    url='https://github.com/khawabonga/dcVirtualSpinning',
+    license='MIT',
+    author='khawabonga',
+    author_email='d.caballero.ok@gmail.com',
+    description='Virtual (Electro)Spinning Microstructure Generator'
+)
---
+++ tests/test_basic.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi!, I\'m test basic!')
+
+
+if __name__ == '__main__':
+    main()
