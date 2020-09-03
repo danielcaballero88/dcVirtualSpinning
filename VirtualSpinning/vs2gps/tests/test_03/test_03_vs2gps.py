8a7577df3a34073753d700d449053c9930ffbda9
Daniel Enrique Caballero <danielcaballero88@gmail.com>
2020-09-03 19:36:02 -0300

    vs2gps tests ok!

--- VirtualSpinning/vs2gps/tests/test_03/test_03_vs2gps.py
+++ VirtualSpinning/vs2gps/tests/test_03/test_03_vs2gps.py
@@ -1,10 +1,22 @@
 from VirtualSpinning.Mallasim.Mallasim import Mallasim 
 from VirtualSpinning.vs2gps import vs2gps
 import os 
+from matplotlib import pyplot as plt
 
 cwd = os.path.dirname(__file__)
 mspath = os.path.join(cwd, "malla_i_s.txt")
 ms = Mallasim.leer_de_archivo(mspath)
+
+
+fig, ax = plt.subplots()
+ms.pre_graficar_bordes(fig, ax)
+colores = ['blue', 'red']
+ms.pre_graficar(fig, ax, cby="lamr",
+                cbar=True, cmap="rainbow",
+                plot_afin=False)
+figpath = os.path.join(cwd, "Mallasim.png")
+fig.savefig(figpath)
+
 
 meshpath = os.path.join(cwd, "Mesh.txt")
 with open(file=meshpath, mode="w") as f: 
